56b050c9fe17d345a4eefe2e215f4db10af7a6bd
Steven Gissubel <steven.gissubel@gmail.com>
2018-02-14 16:25:49 -0500

    added signup/login(reactive form) using material - adding sidenav

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -3,20 +3,32 @@ import {
   MatButtonModule,
   MatIconModule,
   MatFormFieldModule,
-  MatInputModule } from '@angular/material';
+  MatInputModule,
+  MatDatepickerModule,
+  MatNativeDateModule,
+  MatCheckboxModule,
+  MatSidenavModule } from '@angular/material';
 
 @NgModule({
   imports: [
     MatButtonModule,
     MatIconModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatCheckboxModule,
+    MatSidenavModule
   ],
   exports: [
     MatButtonModule,
     MatIconModule,
     MatInputModule,
     MatFormFieldModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatCheckboxModule,
+    MatSidenavModule
   ]
 })
 export class MaterialModule { }
